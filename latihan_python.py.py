0bdf92b04ae8a18927be05ef89396030f6c1f0a4
daaaaaaaam <108453026+daaaaaaaam@users.noreply.github.com>
2024-07-22 12:22:07 +0700

    add files

---
+++ latihan_python.py.py
@@ -0,0 +1,81 @@
+# SOAL 1
+
+# input [1, 2, 3, 4, 5]
+# output [1, 4, 9, 16, 25]
+
+# Buatlah function untuk menerima input lalu mengeluarkan output dengan kuadratu
+def squares(int_list):
+    # Code dibawah sini
+    # --------------------------------
+    pass # hapus pass untuk menjawab
+
+
+# --------------------------------
+
+list_number = [1, 2, 3, 4, 5]
+squares(list_number)
+list_number = [2, 3, 4, 5, 6]
+squares(list_number)
+list_number = [7, 8, 9, 10, 11]
+squares(list_number)
+
+
+# SOAL 2
+
+# Input 90
+# Output A
+# Jika nilai 90 - 100 maka keluarnya A
+# Jika nilai 80 - 89 maka keluarnya B
+# Jika nilai 70 - 79 maka keluarnya C
+# Jika nilai 60 - 69 maka keluarnya Remedial
+
+# Buatlah function untuk mengklasifikasikan nilai siswa berdasarkan persyaratan diatas menggunakan if elif else
+def penilaian_siswa(nilai):
+    # Code dibawah sini
+    # --------------------------------
+    pass # hapus pass untuk menjawab
+
+
+
+    # --------------------------------
+
+nilai_siswa = 90
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 89
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 85
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 80
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 79
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 75
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 70
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 69
+penilaian_siswa(nilai_siswa)
+nilai_siswa = 65
+penilaian_siswa(nilai_siswa)
+
+
+# SOAL 3
+
+# input nama_depan = stephen
+# input nama_belakang = leonardo
+# output Stephen Leonardo
+
+# Buatlah function untuk menerima nama depan dan nama belakang lalu mengeluarkan output nama_lengkap dengan huruf kapital di huruf awal
+def nama_lengkap(nama_depan, nama_belakang):
+    # Code dibawah sini
+    # --------------------------------
+    pass # hapus pass untuk menjawab
+
+    # --------------------------------
+
+nama_depan = "stephen"
+nama_belakang = "leonardo"
+nama_lengkap(nama_depan, nama_belakang)
+nama_depan_1 = "justin"
+nama_belakang_1 = "clooney"
+nama_lengkap(nama_depan_1, nama_belakang_1)
